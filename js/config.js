ce42e28dd7453f6aa03fbf06413a3f88ab46dc97
webzufallsgenerator <zufallsgenerator.web@gmail.com>
2021-05-07 11:57:35 +0200

    config update

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_f6zhO4YHd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1f8lumj4ijm6d3of4k7smg843o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
