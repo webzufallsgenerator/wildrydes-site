378b47d78e5f1da3fae7dbe7dfded45e547518dc
webzufallsgenerator <zufallsgenerator.web@gmail.com>
2021-05-08 12:31:09 +0200

    config update

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://0c9wwrzwyl.execute-api.eu-central-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
